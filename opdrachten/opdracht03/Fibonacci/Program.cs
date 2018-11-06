5bf345206a3924ebf8f984c9ab80602726bc98c7
Simodeme2 <Simodeme2@student.arteveldehs.be>
2018-11-06 14:55:30 +0100

    Opdracht 3

---
+++ opdrachten/opdracht03/Fibonacci/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* int input;
+            Console.Write("1. geef een getal in: ");
+            Console.Write("2. geef een getal in: ");
+
+            input = Convert.ToInt32(Console.ReadLine());*/
+            
+            // Declaratie variabelen
+            int teller, fibo1, fibo2, fibo3;
+            fibo1 = 0;
+            fibo2 = 1;
+
+            // Eerste 2 getallen Fibonacci is 0 en 1
+            Console.WriteLine(fibo1.ToString() + " " + fibo2.ToString() + " ");
+
+            // Fibonacci berekenen tot het 30ste getal
+            for (teller = 3; teller <= 30; teller++)
+            {
+                fibo3 = fibo1 + fibo2;
+                fibo1 = fibo2;
+                fibo2 = fibo3;
+                Console.WriteLine(fibo1);
+                Console.WriteLine(fibo2);
+                Console.WriteLine(fibo3);
+
+                // Weergave
+                Console.Write(fibo3.ToString() + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
